d4d780497459fa40498be0202d18236bb003c8a0
Augustin Le Fèvre <aulefevre@kilix.fr>
2017-10-02 14:41:25 +0200

    Add tests without monolithic

---
+++ src/__tests__/serializerWithoutMonolithic.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createComponent, Provider } from 'react-fela';
+import { createRenderer } from 'fela';
+import renderer from 'react-test-renderer';
+
+import createSerializer from '../serializer';
+
+const Text = createComponent(() => ({ color: 'red', fontSize: '16px' }));
+const Wrapper = createComponent(() => ({
+    border: 'solid 1px black',
+    '> div': {
+        fontWeight: 500,
+    },
+}));
+
+const felaRenderer = createRenderer();
+expect.addSnapshotSerializer(createSerializer(felaRenderer));
+
+it('react-test-renderer', () => {
+    const tree = renderer
+        .create(
+            <Provider renderer={felaRenderer}>
+                <Wrapper>
+                    <Text>Hello World</Text>
+                </Wrapper>
+            </Provider>,
+        )
+        .toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
